463017b374973400aa119f261e938b52f74aa41e
Andrey <andrey.liubashchenko@gmail.com>
2023-03-27 01:55:48 +0300

    sogl

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,28 @@
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmploersList from '../emploters-list/emploers-list';
+import EmploersAddForm from '../emploers-add-form/emploers-add-form';
 import './app.css';
 
+
 function App() {
+    const data = [
+        {name: 'John C.', salary: 800, increase: false, id: 1},
+        {name: 'Alex M.', salary: 3000, increase: true, id: 2},
+        {name: 'Carl W.', salary: 5000, increase: false, id: 3}
+    ]
     return (
         <div className="app">
             <AppInfo/>
 
             <div className="search-panel">
                 <SearchPanel/>
+                <AppFilter/>
+                
             </div>
+                <EmploersList data={data}/>
+                <EmploersAddForm/>
         </div>
     )
 }
